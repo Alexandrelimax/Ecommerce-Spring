b7c1909c384a81259f636ed18d52d74ca63e7320
Alexandre <alexandrelimaxxxx@gmail.com>
2024-06-02 16:37:17 -0300

    Implements TestConfig/Entity User/UserRepository

--- src/main/java/com/course/ecommerce/controller/UserController.java
+++ src/main/java/com/course/ecommerce/controller/UserController.java
@@ -12,11 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/users")
 public class UserController {
 
-    @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
-
-        return new ResponseEntity<>(u1, HttpStatus.OK);
-    }
+//    @GetMapping
+//    public ResponseEntity<User> findAll(){
+//
+//
+//        return new ResponseEntity<>(u1, HttpStatus.OK);
+//    }
 }
--- src/main/java/com/course/ecommerce/repository/UserRepository.java
+++ src/main/java/com/course/ecommerce/repository/UserRepository.java
@@ -1,2 +1,6 @@
-package com.course.ecommerce.repository;public interface UserRepository {
-}
+package com.course.ecommerce.repository;
+
+import com.course.ecommerce.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {}
