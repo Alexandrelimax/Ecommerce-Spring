7f305022025bdc67624362a6706dc2e067be49fb
Alexandre <alexandrelimaxxxx@gmail.com>
2024-06-02 20:37:46 -0300

    Implement findUserById controller/Service

--- src/main/java/com/course/ecommerce/controller/UserController.java
+++ src/main/java/com/course/ecommerce/controller/UserController.java
@@ -1,21 +1,38 @@
 package com.course.ecommerce.controller;
 
 import com.course.ecommerce.entity.User;
+import com.course.ecommerce.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
 
-//    @GetMapping
-//    public ResponseEntity<User> findAll(){
-//
-//
-//        return new ResponseEntity<>(u1, HttpStatus.OK);
-//    }
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        List<User> users = userService.findAll();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Integer id){
+        User user = userService.findById(id);
+        if(user != null){
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
 }
---
+++ src/main/java/com/course/ecommerce/service/UserService.java
@@ -0,0 +1,27 @@
+package com.course.ecommerce.service;
+
+import com.course.ecommerce.entity.User;
+import com.course.ecommerce.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User findById(Integer id){
+        return userRepository.findById(id).orElse(null);
+
+    }
+
+
+}
